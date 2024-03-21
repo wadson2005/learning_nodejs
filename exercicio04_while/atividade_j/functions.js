d6b6e93c1a20d118c240dd72a43be7dce4e1f237
wadson2005 <wadsondias62@gmail.com>
2024-03-21 11:08:50 -0300

    atividade_j created

---
+++ exercicio04_while/atividade_j/functions.js
@@ -0,0 +1,9 @@
+import { question } from "readline-sync";
+
+export function getNumber(msg) {
+    return parseFloat(question(msg))
+}
+
+export function getString(msg) {
+    return question(msg)
+}
